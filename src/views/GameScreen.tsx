b28a735648b1239342835fde686eabc8bf929c71
ehirvi <eino.hirvi@gmail.com>
2025-01-17 17:54:29 +0200

    Add clipboard feature to make inviting players easier

--- src/views/GameScreen.tsx
+++ src/views/GameScreen.tsx
@@ -8,20 +8,71 @@ const StyledGameScreen = styled.div`
   align-items: center;
   flex-direction: column;
   font-family: monospace;
+  gap: 40px;
 `;
 
-const SessionIdText = styled.p`
+const Clipboard = styled.div`
+  display: flex;
+  // align-items: center;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const ClipboardText = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+  margin: 0px;
+`;
+
+const ClipboardBox = styled.input`
+  border: solid 1px;
+  outline: solid 1px;
+  padding: 5px;
+  font-family: monospace;
+  font-size: 15px;
+`;
+
+const CopyButton = styled.button`
+  border: solid black 3px;
+  font-family: monospace;
   font-size: 20px;
+  font-weight: normal;
+  color: black;
+  padding-left: 15px;
+  padding-right: 15px;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  cursor: pointer;
+  box-shadow: 1px 1px 1px 0 purple, 2px 2px 2px 0 black;
+  background-color: lightskyblue;
+
+  &:hover {
+    background-color: rgb(77, 126, 156);
+    transition: 0.2s;
+  }
 `;
 
 const GameScreen = () => {
   const { board, sessionId, handlePlayerMove } = useGameServer();
 
-  if (board) {
+  if (board && sessionId) {
+    const handleButton = () => {
+      void copyToClipboard();
+    };
+
+    // Clipboard API works only in secured HTTPS, can be tested in localhost
+    const copyToClipboard = async () => {
+      await navigator.clipboard.writeText(sessionId);
+    };
+
     return (
       <StyledGameScreen>
         <Grid board={board} makeMove={handlePlayerMove} />
-        <SessionIdText>{sessionId}</SessionIdText>
+        <Clipboard>
+          <ClipboardText>Use this code to invite a player</ClipboardText>
+          <ClipboardBox type="text" readOnly value={sessionId}></ClipboardBox>
+          <CopyButton onClick={handleButton}>Copy to clipboard</CopyButton>
+        </Clipboard>
       </StyledGameScreen>
     );
   }
