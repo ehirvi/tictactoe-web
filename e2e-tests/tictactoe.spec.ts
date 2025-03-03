5bb2e9e0cd2c1d6fc2f0a2d98b7b47f961f47711
ehirvi <eino.hirvi@gmail.com>
2025-03-03 17:31:58 +0200

    Add E2E-tests for the game startup

---
+++ e2e-tests/tictactoe.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Basic functionality of the game", () => {
+  test("Front page opens normally", async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveTitle("Tic-Tac-Toe");
+  });
+
+  test("A new game can be started", async ({ page }) => {
+    await page.goto("/");
+    await page.getByRole("button", { name: "New Game" }).click();
+    await expect(
+      page.getByText("Use this code to invite a player")
+    ).toBeVisible();
+  });
+
+  test("A second player can join an ongoing game", async ({ context }) => {
+    const hostPage = await context.newPage();
+    await hostPage.goto("/");
+    await hostPage.getByRole("button", { name: "New Game" }).click();
+    const gameId = await hostPage.getByRole("textbox").inputValue();
+
+    const guestPage = await context.newPage();
+    await guestPage.goto("/");
+    await guestPage.getByRole("button", { name: "Join Game" }).click();
+    const textBox = guestPage.getByRole("textbox", { name: "Enter Game ID" });
+    await textBox.click();
+    await textBox.fill(gameId);
+    await guestPage.getByRole("button", { name: "Join" }).click();
+    await expect(
+      guestPage.getByText("Use this code to invite a player")
+    ).toBeVisible();
+  });
+});
