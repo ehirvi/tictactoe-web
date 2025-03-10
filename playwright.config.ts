71b99b66bb194a551349e2062e4c680077694ac1
ehirvi <eino.hirvi@gmail.com>
2025-03-10 14:18:20 +0200

    Fix incorrect URL's that caused E2E tests to never start

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3003",
+    baseURL: "http://localhost:3003",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -77,7 +77,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run preview",
-    url: "http://127.0.0.1:3003",
+    url: "http://localhost:3003",
     reuseExistingServer: !process.env.CI,
   },
 });
